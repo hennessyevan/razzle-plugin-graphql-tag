7a7a92e52205084c1deddade775dee5de61a6b64
Evan Hennessy <hennessyevan@users.noreply.github.com>
2021-11-06 13:00:31 -0400

    upgrade for razzle@4

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 const makeLoaderFinder = require("razzle-dev-utils/makeLoaderFinder");
 
-module.exports = function modify(defaultConfig) {
-	const config = Object.assign({}, defaultConfig);
-	config.resolve.extensions.push(".graphql", ".gql");
+'use strict';
+module.exports = {
+  modifyWebpackConfig({
+    webpackConfig
+  }) {
+	webpackConfig.resolve.extensions.push(".graphql", ".gql");
 
-	config.module.rules.push({
+	webpackConfig.module.rules.push({
 		test: /\.(graphql|gql)$/,
 		exclude: /node_modules/,
 		loader: "graphql-tag/loader"
 	});
 
-	config.module.rules[config.module.rules.findIndex(makeLoaderFinder("file-loader"))].exclude.push(/\.(graphql|gql)$/);
+	webpackConfig.module.rules[config.module.rules.findIndex(makeLoaderFinder("file-loader"))].exclude.push(/\.(graphql|gql)$/);
 
-	return config;
+	return webpackConfig;
+  }
 };
